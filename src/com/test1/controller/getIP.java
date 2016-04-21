b6e4a2e7eee42cfd0484d89251ffd27fbbd95991
Dawn <dawn@dxt.cn>
2016-04-21 10:59:39 +0800

    获取本机ip

---
+++ src/com/test1/controller/getIP.java
@@ -0,0 +1,19 @@
+package com.test1.controller;
+
+import java.net.InetAddress;
+
+public class getIP {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InetAddress ia=null;
+		try {
+			ia=ia.getLocalHost();
+			String localname=ia.getHostName();
+			String localip=ia.getHostAddress();
+			System.out.println("本机的ip是 ："+localip);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
